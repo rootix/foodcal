399d72b60b8dd715d2e3128b7605b43fdb04c9d4
Pascal Mathys <pascal.mathys@rootix.ch>
2020-01-04 00:45:17 +0100

    Usability improvements

--- src/app/schedule/components/day-container/day-container.component.ts
+++ src/app/schedule/components/day-container/day-container.component.ts
@@ -1,14 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { isToday } from 'date-fns';
 
 @Component({
     selector: 'fc-day-container',
     templateUrl: './day-container.component.html',
     styleUrls: ['./day-container.component.scss']
 })
-export class DayContainerComponent implements OnInit {
+export class DayContainerComponent {
     @Input() date: Date;
 
-    constructor() {}
-
-    ngOnInit() {}
+    get isToday() {
+        return this.date && isToday(this.date);
+    }
 }
